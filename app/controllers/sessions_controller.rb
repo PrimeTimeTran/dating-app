071c495a36c3fd95505770c4f0c64446729ac29d
Duy Phuc <phuc.tran@yomi.studio>
2017-06-21 20:28:31 +0700

    Add sessioncontroller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    if @user = User.find_by(email: params[:email])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to users_path
+      else
+        flash[:errors] = "Wrong password"
+        render 'new'
+      end
+    else
+      flash[:errors] = "Email not found"
+      render 'new'
+    end
+  end
+end
