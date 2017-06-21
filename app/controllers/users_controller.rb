26f6c8ae4a704c5922d96399b52314a9dc20958f
Duy Phuc <phuc.tran@yomi.studio>
2017-06-21 20:27:13 +0700

    Add sign in and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+    @users = User.all
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Sign up successful"
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:error] = "Error #{@user.errors.full_messages.to_sentence}"
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
 end
